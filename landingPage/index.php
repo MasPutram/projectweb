b67c0ba6e78572cd8b0567645dfdc129119b68d5
Nur Ikhlas Putra Ramadhan <putramadhan010@gmail.com>
2024-07-27 20:03:34 +0700

    initialize commit

---
+++ landingPage/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4>Haloo</h4>
+    <p>ini project git pertama saya ,<br>sekedar untuk coba coba saja</p>
+</body>
+</html>
